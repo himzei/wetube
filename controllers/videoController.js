8579d378598f88e0f3b275e66fc63c2b57bc3d40
himzei <himzei@himzeiui-MacBookPro.local>
2021-01-16 19:55:50 +0900

    2.18

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,3 +1,10 @@
-export const home = (req, res) => res.send("Home");
-export const search = (req, res) => res.send("search");
-
+export const home = (req, res) => res.render("home", {pageTitle: "Home"});
+export const search = (req, res) => {
+    const {query: { term : searchingBy }} = req;
+    res.render("search", {pageTitle: "Search", searchingBy});
+}
+export const videos = (req, res) => res.render("videos", {pageTitle: "Videos"})
+export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"})
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Detail Video"})
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"}) 
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"}) 
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
 import routes from "../routes";
+import { users, userDetail, editProfile, changePassword } from "../controllers/userController";
 
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, (req, res) => res.send("users"));
-userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
-userRouter.get(routes.editProfile, (req, res) => res.send("user Edit"));
-userRouter.get(routes.changePassword, (req, res) => res.send("change password"));
+userRouter.get(routes.users, users)
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
