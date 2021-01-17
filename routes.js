9a88db53a4df4e000e7a2b413ebab60bf777b9ab
himzei <himzei@himzeiui-MacBookPro.local>
2021-01-17 09:34:05 +0900

    2.21

--- routes.js
+++ routes.js
@@ -5,36 +5,36 @@ const LOGIN = "/login";
 const LOGOUT = "/logout";
 const SEARCH = "/search";
 
-// Users 
+// Users
+
 const USERS = "/users";
 const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
+// Videos
 
-// Videos 
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
-const DELETE_VIDEO = "/:id/delete"
-
+const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
-    home: HOME, 
-    join: JOIN, 
-    login: LOGIN, 
-    logout: LOGOUT, 
-    search: SEARCH, 
-    users: USERS, 
-    userDetail: USER_DETAIL, 
-    editProfile: EDIT_PROFILE, 
-    changePassword: CHANGE_PASSWORD, 
-    videos: VIDEOS, 
-    upload: UPLOAD, 
-    videoDetail: VIDEO_DETAIL, 
-    editVideo: EDIT_VIDEO, 
-    deleteVideo: DELETE_VIDEO
-}
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: USER_DETAIL,
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: VIDEO_DETAIL,
+  editVideo: EDIT_VIDEO,
+  deleteVideo: DELETE_VIDEO
+};
 
 export default routes;
