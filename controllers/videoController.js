6df74dac18014505326e51b876c8b61ceb003720
himzei <himzei@himzeiui-MacBookPro.local>
2021-01-18 08:44:37 +0900

    2.25

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes"; 
 
 export const home = (req, res) => res.render("home", {pageTitle: "Home", videos});
 export const search = (req, res) => {
@@ -6,7 +7,18 @@ export const search = (req, res) => {
     res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
 
-export const upload = (req, res) => res.render("upload", {pageTitle: "Upload"})
+export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"})
+export const postUpload = (req, res) => {
+    const{
+        body :{
+            file, 
+            title, 
+            description
+        }
+    } = req;
+    res.redirect(routes.videoDetail(55555))
+}
+
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Detail Video"})
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: "Edit Video"}) 
 export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: "Delete Video"}) 
--- middlewares.js
+++ middlewares.js
@@ -2,7 +2,11 @@ import routes from "./routes";
 
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "weTube";
-    res.locals.routes = routes
+    res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true, 
+        id: 1
+    }
     next();   
 
 }
