22966df3fd9655d678289438c0dc60c34924c6e7
himzei <himzei@himzeiui-MacBookPro.local>
2021-01-18 14:48:37 +0900

    3.7 Uploading and creating a video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,21 +1,34 @@
 import routes from "../routes"; 
+import Video from "../models/Video";
 
-export const home = (req, res) => res.render("home", {pageTitle: "Home", videos});
+export const home = async (req, res) => {
+    try{
+        const videos = await Video.find({});
+        
+        res.render("home", {pageTitle: "Home", videos});
+    }catch(error){
+        console.log(error);
+        res.render("home", {pageTitle: "Home", videos: []})
+    }
+}
 export const search = (req, res) => {
     const {query: { term : searchingBy }} = req;
     res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
 
 export const getUpload = (req, res) => res.render("upload", {pageTitle: "Upload"})
-export const postUpload = (req, res) => {
-    const{
-        body :{
-            file, 
-            title, 
-            description
-        }
+export const postUpload = async (req, res) => {
+    const{ 
+        body: {title, description }, 
+        file: { path } 
     } = req;
-    res.redirect(routes.videoDetail(55555))
+    const newVideo = await Video.create({
+        fileUrl: path, 
+        title, 
+        description
+    })
+    console.log(newVideo);
+    res.redirect(routes.videoDetail(newVideo.id));
 }
 
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: "Detail Video"})
