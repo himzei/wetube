68f7efdaba49f60654cb3249036c59d32b3ba807
himzei <himzei@himzeiui-MacBookPro.local>
2021-01-18 19:34:09 +0900

    4.3 ES6 with webpack

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
     try{
-        const videos = await Video.find({});
+        const videos = await Video.find({}).sort({"_id": -1});
         
         res.render("home", {pageTitle: "Home", videos});
     }catch(error){
@@ -11,8 +11,14 @@ export const home = async (req, res) => {
         res.render("home", {pageTitle: "Home", videos: []})
     }
 }
-export const search = (req, res) => {
+export const search = async (req, res) => {
     const {query: { term : searchingBy }} = req;
+    let videos = []
+    try{
+        videos = await Video.find({title: {$regex: searchingBy, $options: "i"}})
+    }catch(error){
+        console.log(error)
+    }
     res.render("search", {pageTitle: "Search", searchingBy, videos});
 }
 
@@ -77,7 +83,7 @@ export const deleteVideo = async (req, res) => {
     try {
         await Video.findOneAndRemove({_id:id})
     }catch(error) {
-        
+        console.log(error);
     }
     res.redirect(routes.home)
 }
