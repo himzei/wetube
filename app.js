a9c7836ddb9577452729b0289e659b5947ec8d96
himzei <himzei@himzeiui-MacBookPro.local>
2021-01-19 10:28:42 +0900

    6.5 MongoStore and middleware

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport"
+import session from "express-session";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes"
 import { localsMiddleware } from "./middlewares";
 
+import "./passport";
+
 
 const app = express()
+
 
 app.use(helmet());
 app.use(function(req, res, next) {
@@ -20,12 +25,19 @@ app.use(function(req, res, next) {
 app.set("view engine", "pug");
 app.use("/uploads", express.static("uploads"))
 app.use("/static", express.static("static"))
-app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan("dev"))
+app.use(session({
+    secret: process.env.COOKIE_SECRET, 
+    resave: true, 
+    saveUninitialized: false
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(localsMiddleware)
+app.use(cookieParser());
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
